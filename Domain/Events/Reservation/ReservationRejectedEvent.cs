05d8a70cf61c439db4b86b92cd17ffc6136697fb
Benjamin <lolgame99@gmail.com>
2024-06-22 11:37:03 +0200

    Refactored Reservation LimitExceeded & Rejected Events to reflect new changes

--- Domain/Events/Reservation/ReservationRejectedEvent.cs
+++ Domain/Events/Reservation/ReservationRejectedEvent.cs
@@ -1,7 +1,22 @@
+using System.Text.Json.Serialization;
 using MediatR;
 
 namespace PlayOfferService.Domain.Events.Reservation;
 
 public class ReservationRejectedEvent : DomainEvent
 {
+    [JsonPropertyName("start")]
+    public DateTime Start { get; set; }
+    [JsonPropertyName("end")]
+    public DateTime End { get; set; }
+    [JsonPropertyName("reservantId")]
+    public Guid? ReservantId { get; set; }
+    [JsonPropertyName("tournamentId")]
+    public Guid? TournamentId { get; set; }
+    [JsonPropertyName("participantIds")]
+    public List<Guid>? ParticipantIds { get; set; }
+    [JsonPropertyName("courtIds")]
+    public Guid CourtId { get; set; }
+    
+    public ReservationRejectedEvent(){}
 }
