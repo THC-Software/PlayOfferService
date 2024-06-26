ed7241fd4c5dbab9533d6bde05e1a4f508a2555d
CengoleFHV <cengizhan.aydin@students.fhv.at>
2024-06-26 11:15:14 +0200

    added ConcurrencyCheck for CancelPlayOfferEvent

--- Application/Handlers/CancelPlayOfferHandler.cs
+++ Application/Handlers/CancelPlayOfferHandler.cs
@@ -1,7 +1,6 @@
 using MediatR;
 using PlayOfferService.Application.Commands;
 using PlayOfferService.Application.Exceptions;
-using PlayOfferService.Domain;
 using PlayOfferService.Domain.Events;
 using PlayOfferService.Domain.Events.PlayOffer;
 using PlayOfferService.Domain.Models;
@@ -14,7 +13,7 @@ public class CancelPlayOfferHandler : IRequestHandler<CancelPlayOfferCommand, Ta
     private readonly WriteEventRepository _writeEventRepository;
     private readonly PlayOfferRepository _playOfferRepository;
     private readonly ClubRepository _clubRepository;
-    
+
     public CancelPlayOfferHandler(WriteEventRepository writeEventRepository, PlayOfferRepository playOfferRepository, ClubRepository clubRepository)
     {
         _writeEventRepository = writeEventRepository;
@@ -24,22 +23,25 @@ public class CancelPlayOfferHandler : IRequestHandler<CancelPlayOfferCommand, Ta
 
     public async Task<Task> Handle(CancelPlayOfferCommand request, CancellationToken cancellationToken)
     {
+        var transaction = _writeEventRepository.StartTransaction();
+        var excpectedEventCount = _writeEventRepository.GetEventCount(request.PlayOfferId) + 1;
+
         var existingPlayOffer = (await _playOfferRepository.GetPlayOffersByIds(request.PlayOfferId)).FirstOrDefault();
         if (existingPlayOffer == null)
             throw new NotFoundException($"PlayOffer {request.PlayOfferId} not found!");
         if (existingPlayOffer.CreatorId != request.MemberId)
             throw new AuthorizationException($"PlayOffer {request.PlayOfferId} can only be cancelled by creator!");
-        
+
         if (existingPlayOffer.OpponentId != null)
             throw new InvalidOperationException($"PlayOffer {request.PlayOfferId} is already accepted and cannot be cancelled!");
         if (existingPlayOffer.IsCancelled)
             throw new InvalidOperationException($"PlayOffer {request.PlayOfferId} is already cancelled!");
-        
+
         var existingClub = await _clubRepository.GetClubById(existingPlayOffer.ClubId);
         if (existingClub == null)
             throw new NotFoundException($"Club {existingPlayOffer.ClubId} not found!");
-        
-        
+
+
         switch (existingClub.Status)
         {
             case Status.LOCKED:
@@ -47,7 +49,7 @@ public class CancelPlayOfferHandler : IRequestHandler<CancelPlayOfferCommand, Ta
             case Status.DELETED:
                 throw new InvalidOperationException("Can't cancel PlayOffer in deleted club!");
         }
-        
+
         var domainEvent = new BaseEvent
         {
             EntityId = request.PlayOfferId,
@@ -57,9 +59,20 @@ public class CancelPlayOfferHandler : IRequestHandler<CancelPlayOfferCommand, Ta
             EventData = new PlayOfferCancelledEvent(),
             Timestamp = DateTime.UtcNow
         };
-        
+
         await _writeEventRepository.AppendEvent(domainEvent);
         await _writeEventRepository.Update();
+    
+        
+        var eventCount = _writeEventRepository.GetEventCount(request.PlayOfferId);
+        
+        if (eventCount != excpectedEventCount)
+        {
+            transaction.Rollback();
+            throw new InvalidOperationException("Concurrent modification detected!");
+        }
+        
+        transaction.Commit();
         
         return Task.CompletedTask;
     }
